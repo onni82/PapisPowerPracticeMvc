987ca91b7ef3f69c6c5f850cccd79cfd996f0ec5
Korkken <Korkken@gmail.com>
2025-10-09 10:24:28 +0200

    added test view to workoutlog

---
+++ Controllers/WorkoutLogController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PapisPowerPracticeMvc.Models;
+using System.Diagnostics;
+
+namespace PapisPowerPracticeMvc.Controllers
+{
+    public class WorkoutLogController : Controller
+    {
+        private readonly ILogger<WorkoutLogController> _logger;
+
+        public WorkoutLogController(ILogger<WorkoutLogController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult WorkoutLog()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
