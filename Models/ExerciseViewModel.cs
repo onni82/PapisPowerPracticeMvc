df9ef683b9e7d6ee508c5ec58ccfcfd9f5fc6354
onni82 <onni.bucht@me.com>
2025-10-15 15:32:43 +0200

    Added view model

---
+++ Models/ExerciseViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace PapisPowerPracticeMvc.Models
+{
+    public class ExerciseViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public string? VideoUrl { get; set; }
+    }
+}
