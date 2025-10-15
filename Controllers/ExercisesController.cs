65b2f3932452802e96b13ffca9b94b3c0cbaf4c3
onni82 <onni.bucht@me.com>
2025-10-15 15:34:51 +0200

    Added ExercisesController

---
+++ Controllers/ExercisesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PapisPowerPracticeMvc.Controllers
+{
+    public class ExercisesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
