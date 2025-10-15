821c21b25ce98f9e25883552618cf7fecda70354
onni82 <onni.bucht@me.com>
2025-10-15 15:51:17 +0200

    Added Details() to ExercisesController

--- Controllers/ExercisesController.cs
+++ Controllers/ExercisesController.cs
@@ -1,12 +1,26 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using PapisPowerPracticeMvc.Models;
 
 namespace PapisPowerPracticeMvc.Controllers
 {
     public class ExercisesController : Controller
     {
-        public IActionResult Index()
+        private readonly HttpClient _httpClient;
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public ExercisesController(HttpClient httpClient, IHttpContextAccessor httpContextAccessor)
         {
-            return View();
+            _httpClient = httpClient;
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        public async Task<IActionResult> Details(int id)
+        {
+            var response = await _httpClient.GetFromJsonAsync<ExerciseViewModel>($"api/exercises/{id}");
+            if (response == null)
+                return NotFound();
+
+            return View(response);
         }
     }
 }
