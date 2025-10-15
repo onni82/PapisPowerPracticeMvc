de1a5a8a90ec4fe94c4a8a6d7c9880cf19691585
onni82 <onni.bucht@me.com>
2025-10-15 16:02:09 +0200

    Added GetExerciseByIdAsync()

---
+++ Data/Services/IService/IExerciseService.cs
@@ -0,0 +1,10 @@
+﻿using PapisPowerPracticeMvc.Models;
+using PapisPowerPracticeMvc.ViewModels;
+
+namespace PapisPowerPracticeMvc.Data.Services.IService
+{
+    public interface IExerciseService
+    {
+        Task<ExerciseViewModel?> GetExerciseByIdAsync(int id);
+    }
+}
