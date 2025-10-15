3e3a4c242b24eb8f8b8afec7b16f66b51f832f1a
edwin Torres <edwintorres@live.se>
2025-10-15 15:29:12 +0200

    Fixed Auth controllers and service to work with my login and logout views

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -76,11 +76,14 @@ namespace PapisPowerPracticeMvc.Controllers
             return RedirectToAction("Index", "Home");
         }
 
-        //[HttpPost]
-        //public async Task<IActionResult> Logout()
-        //{
-           
+        [HttpPost]
+        public async Task<IActionResult> Logout()
+        {
 
-        //}
+            await _authService.LogoutUser();
+
+            return RedirectToAction("Index", "Home");
+
+        }
     }
 }
