bd55a26552c0d5e06f0860cb42904b654bef688f
onni82 <onni.bucht@me.com>
2025-10-15 16:05:42 +0200

    Implemented GetExerciseByIdAsync(int id)

---
+++ Data/Services/ExerciseService.cs
@@ -0,0 +1,20 @@
+﻿using PapisPowerPracticeMvc.Data.Services.IService;
+using PapisPowerPracticeMvc.ViewModels;
+
+namespace PapisPowerPracticeMvc.Data.Services
+{
+    public class ExerciseService : IExerciseService
+    {
+        private readonly HttpClient _httpClient;
+
+        public ExerciseService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<ExerciseViewModel?> GetExerciseByIdAsync(int id)
+        {
+            return await _httpClient.GetFromJsonAsync<ExerciseViewModel>($"exercises/{id}");
+        }
+    }
+}
