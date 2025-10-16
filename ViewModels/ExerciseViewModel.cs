d479a3b99094717a4acca76ee57e99816d874164
onni82 <onni.bucht@me.com>
2025-10-16 11:36:28 +0200

    Initialize as new()

--- ViewModels/ExerciseViewModel.cs
+++ ViewModels/ExerciseViewModel.cs
@@ -6,6 +6,6 @@
         public string Name { get; set; } = string.Empty;
         public string Description { get; set; } = string.Empty;
         public string? VideoUrl { get; set; }
-        public List<string>? MuscleGroups { get; set; }
+        public List<string>? MuscleGroups { get; set; } = new();
     }
 }
