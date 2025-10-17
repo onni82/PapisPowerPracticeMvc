24b26613e2d55d5fded0bb90bade1141fd0a9964
onni82 <onni.bucht@me.com>
2025-10-17 09:31:40 +0200

    fixed import

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,6 +1,6 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
-using PapisPowerPracticeMvc.Models;
+using PapisPowerPracticeMvc.ViewModels;
 
 namespace PapisPowerPracticeMvc.Controllers
 {
