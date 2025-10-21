bbdd8138270177da92de907e178380843a285d6a
onni82 <onni.bucht@me.com>
2025-10-21 11:37:10 +0200

    Added MuscleGroupViewModel

---
+++ ViewModels/MuscleGroupViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace PapisPowerPracticeMvc.ViewModels
+{
+    public class MuscleGroupViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public List<ExerciseViewModel>? Exercises { get; set; }
+    }
+}
