833e2488f362bbe39e5340b9244d12027c37267c
onni82 <onni.bucht@me.com>
2025-10-21 15:02:31 +0200

    Created service interface

---
+++ Data/Services/IService/IMuscleGroupService.cs
@@ -0,0 +1,10 @@
+﻿using PapisPowerPracticeMvc.ViewModels;
+
+namespace PapisPowerPracticeMvc.Data.Services.IService
+{
+    public interface IMuscleGroupService
+    {
+        Task<IEnumerable<MuscleGroupViewModel>> GetAllMuscleGroupsAsync();
+        Task<MuscleGroupViewModel?> GetMuscleGroupByIdAsync(int id);
+    }
+}
