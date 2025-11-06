8a4f5f78bf207b90b910d6da6ada325c87c50a69
onni82 <onni.bucht@me.com>
2025-11-06 11:25:41 +0100

    Made ImageUrl required and added error messages

--- ViewModels/MuscleGroupViewModel.cs
+++ ViewModels/MuscleGroupViewModel.cs
@@ -1,9 +1,14 @@
-﻿namespace PapisPowerPracticeMvc.ViewModels
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PapisPowerPracticeMvc.ViewModels
 {
     public class MuscleGroupViewModel
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "Namn är obligatoriskt")]
         public string Name { get; set; } = string.Empty;
-        public string? ImageUrl { get; set; }
+        [Required(ErrorMessage = "URL är obligatorisk")]
+        [Url(ErrorMessage = "Ange en giltig URL")]
+        public string ImageUrl { get; set; } = string.Empty;
     }
 }
