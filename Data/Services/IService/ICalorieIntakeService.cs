e65f6bec170533f52c517db4d28796097525086f
onni82 <onni.bucht@me.com>
2025-11-10 09:31:23 +0100

    Added ICalorieIntakeService

---
+++ Data/Services/IService/ICalorieIntakeService.cs
@@ -0,0 +1,10 @@
+﻿using PapisPowerPracticeMvc.Models;
+
+namespace PapisPowerPracticeMvc.Data.Services.IService
+{
+    public interface ICalorieIntakeService
+    {
+        Task<IEnumerable<string>> GetActivityLevelsAsync();
+        Task<CalorieResultViewModel?> CalculateAsync(CalorieDataViewModel model);
+    }
+}
