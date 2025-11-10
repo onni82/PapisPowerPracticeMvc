0d7165f4e7dc25879afb0c4d598e26b6f2883d47
onni82 <onni.bucht@me.com>
2025-11-10 09:29:10 +0100

    Added models

---
+++ Models/CalorieResultViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace PapisPowerPracticeMvc.Models
+{
+    public class CalorieResultViewModel
+    {
+        public string Gender { get; set; } = "";
+        public string ActivityLevel { get; set; } = "";
+        public double Bmr { get; set; }
+        public double Tdee { get; set; }
+    }
+}
